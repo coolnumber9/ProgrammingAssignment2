b44f210334165b327472160597d355d23326fc63
Kristoffer Dominic Amora <kdamora@gmail.com>
2017-12-07 03:05:24 +0800

    Initial commit for solving the inverse of the matrix passed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are functions completed as part of the Coursera Programming 
+## Assignment 2 (Peer-graded) : Lexical Scoping. 
 
-## Write a short comment describing this function
+## The two functions below revolves around the concept of caching
+## in order to make time-consuming computations faster instead of
+## repeating the computation.
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix function creates a special matrix object that can
+## cache its inverse.
 
+makeCacheMatrix <- function(origMat = matrix()) {
+
+	invMat <- NULL ## this is also to identify if it's cached already or not...
+
+	## Function to save the original matrix to another matrix variable
+	makeMatrix <- function(matrix) {
+		origMat <<- matrix
+		invMat <<- NULL ## make sure theres no random value
+	}
+
+	## Function that returns the original matrix passed as an argument
+	retMat <- function() origMat
+
+	## Funtion that sets the inverse of the matrix
+	doInverseMat <- function(inverse) invMat <<- inverse
+
+	## Function that returs the inverse of the matrix
+	retInverseMat <- function() invMat
+
+	## Last execution to return the following lists...
+	list (makeMatrix = makeMatrix, 
+		retMat = retMat, 
+		doInverseMat = doInverseMat,
+		retInverseMat = retInverseMat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special matrix returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cachesolve should retrieve the 
+## inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(origMat, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    sMat <- origMat$retInverseMat()
+
+    if(!is.null(origMat)) {
+    	return(origMat)
+    }
+
+    dat_to_be_solved <- origMat$retMat()
+
+    sMat <- solve(dat_to_be_solved, ...)
+
+    origMat$doInverseMat(sMat)
+
+    sMat
 }
